828690e1d3b3b65437af92c51397d97574ebfcc7
kadyrkaragishiev@gmail.com <kadyrkaragishiev@gmail.com>
2019-01-11 12:02:23 +0300

    menu

---
+++ Assets/MyAssets/Scripts/MenuScene/MenuHand.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHand : MonoBehaviour
+{
+    public void Escape_from_fire()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Tutorial_scene_for_fire()
+    {
+        Debug.Log("tutor");
+    }
+}
